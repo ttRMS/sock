e869b23cf3ec45b535e196ace50c3f4e9216af30
tycrek <josh.moore@jmoore.dev>
2022-01-26 14:29:22 -0700

    maybe fix weird parameters

--- src/main/java/io/ttrms/sock/Request.java
+++ src/main/java/io/ttrms/sock/Request.java
@@ -11,11 +11,11 @@ public class Request {
 
     public Request(String... args) {
         this.route = args[0];
-        this.args = List.of(args).subList(1, args.length).toArray(new String[0]);
+        this.args = args.length > 1 ? List.of(args).subList(1, args.length).toArray(new String[0]) : new String[0];
     }
 
     public Request(List<String> args) {
         this.route = args.get(0);
-        this.args = args.subList(1, args.size()).toArray(new String[0]);
+        this.args = args.size() > 1 ? args.subList(1, args.size()).toArray(new String[0]) : new String[0];
     }
 }
