4168fc58f82fa1871ff7f6426f25536d77cf2077
tycrek <josh.moore@jmoore.dev>
2022-02-01 12:36:22 -0700

    Added Javadoc to Request

--- src/main/java/io/ttrms/sock/Request.java
+++ src/main/java/io/ttrms/sock/Request.java
@@ -9,11 +9,19 @@ public class Request {
     private final String route;
     private final String[] args;
 
+    /**
+     * Create a new <code>Request</code> object.
+     * The first argument is the request route, the rest are the request arguments.
+     */
     public Request(String... args) {
         this.route = args[0];
         this.args = args.length > 1 ? List.of(args).subList(1, args.length).toArray(new String[0]) : new String[0];
     }
 
+    /**
+     * Create a new <code>Request</code> object.
+     * The first argument is the request route, the rest are the request arguments.
+     */
     public Request(List<String> args) {
         this.route = args.get(0);
         this.args = args.size() > 1 ? args.subList(1, args.size()).toArray(new String[0]) : new String[0];
